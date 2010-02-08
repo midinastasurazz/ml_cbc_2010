ceb3cc565c5c66755bcabe86b0f0ce31c08660b5
Martin Pavlovsky <mp107@gondor.doc.ic.ac.uk>
2010-02-08 13:48:14 +0000

    Function to calculate recall, precision and fMeasure added. Screenshots of trees, precison, recall and fMeasure saved

--- src/our/assign1/runEverything.m
+++ src/our/assign1/runEverything.m
@@ -6,4 +6,9 @@ n = 10;
 
 [totalError, percentageError, confusionM] = nFold2(examples, targets, n);
 
+classifications = testAllTrees(trees,examples);
+actualM = confusion_matrix(classifications, targets);
+
+[recall precision fMeasure] = calculateRecallPrecision(confusionM, actualM, 1);
+
 sprintf('Total error estimate: %d', totalError);
